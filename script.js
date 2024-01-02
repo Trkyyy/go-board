21a989b550ab62b0e52e4bdab8277adecfd466d1
Turkey <spearzyy@gmail.com>
2024-01-02 17:07:29 +0000

    Add controls

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var board = new JosekiTraining.Board({
-      container: '#board',
-      size: 19,
-      theme: 'wooden'
-    });
-  });
+var boardElement = document.querySelector(".tenuki-board");
+
+//localStorage.setItem("startPath", JSON.stringify([{y:3,x:15}, {pass:true}, {y:2,x:13}]));
+//var game = new tenuki.Game({ element: boardElement }, localStorage);
+
+var game = new tenuki.Game({ element: boardElement });
+
+game.setAutoplay("black"); // AI is white
+
+var controlElement = document.querySelector(".controls");
+var controls = new ExampleGameControls(controlElement, game);
+controls.setup();
+
+game.callbacks.postRender = function(game) {
+  controls.updateStats();
+};
+
+document.addEventListener("keydown",  function (e) {
+      //if (e.keyCode == 82) { // r pressed
+      if (e.keyCode == 69) { // e pressed
+        controls.reset(e);
+      }
+      else if (e.keyCode == 27) { // enter pressed
+        controls.reset(e);
+      }
+   }, false);
